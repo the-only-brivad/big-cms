d357a47990c1f5eb99b745a0e73fc0ea51927b8c
BVadasz <brianvadasz@quickenloans.com>
2020-09-27 13:03:42 -0400

    Updating Entries

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 9000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '3d114a727e122692b44db918ac24ba3e'),
